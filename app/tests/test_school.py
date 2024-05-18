52be385d293cb1e17140f0d26aed1efa0d7d6952
pixelead0 <adangq@gmail.com>
2024-05-17 23:32:53 -0600

    porfin termine el CRUD de escuelas con pruebas y autenticacion JWT

--- app/tests/test_school.py
+++ app/tests/test_school.py
@@ -61,13 +61,14 @@ def test_create_school(auth_headers):
     assert "id" in data
 
 
-def test_read_schools():
-    response = client.get("/api/schools/")
+def test_read_schools(auth_headers):
+    response = client.get("/api/schools/", headers=auth_headers)
+    logger.info(response.__dict__)
     assert response.status_code == 200
     assert len(response.json()) > 0
 
 
-def test_read_school():
+def test_read_school(auth_headers):
     school_id = client.post(
         "/api/schools/",
         json={
@@ -75,15 +76,15 @@ def test_read_school():
             "country": "Francia",
             "state": "Paris",
             "description": "La mejor escuela del mundo europeo",
-        },
+        }, headers=auth_headers
     ).json()["id"]
-    response = client.get(f"/api/schools/{school_id}")
+    response = client.get(f"/api/schools/{school_id}", headers=auth_headers)
     assert response.status_code == 200
     data = response.json()
     assert data["id"] == school_id
 
 
-def test_update_school():
+def test_update_school(auth_headers):
     school_id = client.post(
         "/api/schools/",
         json={
@@ -91,7 +92,7 @@ def test_update_school():
             "country": "Muy Lejano",
             "state": "Lejos",
             "description": "Escuela que esta donde da vuelta el aire",
-        },
+        }, headers=auth_headers
     ).json()["id"]
     response = client.put(
         f"/api/schools/{school_id}",
@@ -100,14 +101,14 @@ def test_update_school():
             "country": "Muy cerca",
             "state": "Cerquita",
             "description": "Escuela que esta donde pasando la calle",
-        },
+        }, headers=auth_headers
     )
     assert response.status_code == 200
     data = response.json()
     assert data["name"] == "Escuela cerca"
 
 
-def test_delete_school():
+def test_delete_school(auth_headers):
     school_id = client.post(
         "/api/schools/",
         json={
@@ -115,12 +116,12 @@ def test_delete_school():
             "country": "cerca",
             "state": "Aqui",
             "description": "Escuela que esta aqui",
-        },
+        }, headers=auth_headers
     ).json()["id"]
-    response = client.delete(f"/api/schools/{school_id}")
+    response = client.delete(f"/api/schools/{school_id}", headers=auth_headers)
     assert response.status_code == 200
     data = response.json()
     assert data["id"] == school_id
     # Verify that the school is actually deleted
-    response = client.get(f"/schools/{school_id}")
+    response = client.get(f"/api/schools/{school_id}", headers=auth_headers)
     assert response.status_code == 404
